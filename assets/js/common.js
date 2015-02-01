33daf102cde0af4f058c604ae54c38cbf2de7b30
Romany Nassief <rnassief@see3.com>
2015-02-01 19:58:55 +0200

    Initial setup

---
+++ assets/js/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+   
+});
+
+
+function isTouchDevice() {
+    return (('ontouchstart' in window)
+            || (navigator.MaxTouchPoints > 0)
+            || (navigator.msMaxTouchPoints > 0));
+}
